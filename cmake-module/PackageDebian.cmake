62c1d38058e1cfd5c096ed34e0b16f0ba69425b3
Lars Gunnarsson <gunnarsson.lars@gmail.com>
2021-07-16 10:22:16 +0200

    Update debian package dependency

--- cmake-module/PackageDebian.cmake
+++ cmake-module/PackageDebian.cmake
@@ -1,7 +1,7 @@
 set(CPACK_GENERATOR "DEB")
 set(CPACK_PACKAGE_CONTACT "Lars Gunnarsson <gunnarsson.lars@gmail.com>")
 set(CPACK_DEBIAN_PACKAGE_HOMEPAGE "${PROJECT_HOMEPAGE_URL}")
-set(CPACK_DEBIAN_PACKAGE_DEPENDS "libssl | libssl-dev")
+set(CPACK_DEBIAN_PACKAGE_DEPENDS "libssl1.1 | libssl-dev")
 set(CPACK_DEBIAN_PACKAGE_CONTROL_EXTRA
   "${PROJECT_SOURCE_DIR}/debian/script/postinst"
   "${PROJECT_SOURCE_DIR}/debian/script/conffiles"
